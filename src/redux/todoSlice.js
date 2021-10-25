a77088cf019c13d0c973aa076f0256ec6c352a4a
gidwikintan <gidwikintan@gmail.com>
2021-10-26 06:59:02 +0700

    fetchin todos from API

--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -1,4 +1,13 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+export const getTodoAsync = createAsyncThunk('todos/getTodosAsync',
+    async () => {
+        const response = await fetch('http://localhost:7000/todos')
+        if (response.ok) {
+            const todos = await response.json()
+            return {todos}
+        }
+})
 
 const todoSlice = createSlice({
     name: "todos",
@@ -24,6 +33,11 @@ const todoSlice = createSlice({
         },
         deleteTodo: (state, action) => {
             return state.filter((todo)=>todo.id !== action.payload.id)
+        },
+    },
+    extraReducers: {
+        [getTodoAsync.fulfilled]: (state, action) => {
+            return action.payload.todos;
         }
     }
 })
